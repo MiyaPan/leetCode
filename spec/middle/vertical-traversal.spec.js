b0b38f38767223a36867fdeed376233e22e42a1c
panwenshuang <panwenshuang@baidu.com>
2020-06-14 17:54:41 +0800

    二叉树垂直遍历

---
+++ spec/middle/vertical-traversal.spec.js
@@ -0,0 +1,15 @@
+
+import {verticalTraversal} from '../../src/middle/vertical-traversal';
+import {createTreeByLevelTraseval} from '../../src/utils/binaryTree';
+
+describe('verticalTraversal', () => {
+    const tree = createTreeByLevelTraseval([3,9,20,null,null,15,7]);
+
+    test("verticalTraversal return", () => {
+        expect(verticalTraversal(tree)).toEqual([[9],[3,15],[20],[7]]);
+    });
+    const tree1 = createTreeByLevelTraseval([0,5,1,9,null,2,null,null,null,null,3,4,8,6,null,null,null,7]);
+    // test("verticalTraversal return", () => {
+    //     expect(verticalTraversal(tree1)).toEqual([[9,7],[5,6],[0,2,4],[1,3],[8]]);
+    // });
+});
